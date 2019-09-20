df01dcf55761694bcfe8b9cc3ed8f68c5c80c9ea
yubukedong <2826787391@qq.com>
2019-09-20 10:38:22 +0800

    分支gjy

--- app/src/main/java/com/example/daytwo/MainActivity.java
+++ app/src/main/java/com/example/daytwo/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		int four = 1354;
+		int five = 89512;
+		int six = 655468;
     }
 }
